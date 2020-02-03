c163d2ef74490b06cd5c8d27aca33e7c3b6a5619
Company <893042011@qq.com>
2020-02-03 17:17:32 +0800

    add recommend

---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,92 @@
+<template>
+  <div>
+    <div class="title">热销推荐</div>
+    <ul>
+      <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+        <img class="item-img" :src="item.imgUrl">
+        <div class="item-info">
+          <p class="item-title">{{ item.title }}</p>
+          <p class="item-desc">{{ item.desc }}</p>
+          <button class="item-button">查看详情</button>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeRecommend',
+  data () {
+    return {
+      recommendList: [{
+        id: '001',
+        imgUrl: '//imgs.qunarzz.com/vs_ceph_vs_tts/e802ce19-7950-419c-b790-39fb364cf7f6.jpg_r_480x320x90_b8313d0d.jpg',
+        title: '北海当地游 6天自由行',
+        desc: '🔥火爆海岛游⊙北海+涠洲岛6日⊙赠赶海+海底世界/住香格里拉+船票+上岛费'
+
+      },
+      {
+        id: '002',
+        imgUrl: '//imgs.qunarzz.com/vs_ceph_vs_tts/e802ce19-7950-419c-b790-39fb364cf7f6.jpg_r_480x320x90_b8313d0d.jpg',
+        title: '北海当地游 6天自由行',
+        desc: '🔥火爆海岛游⊙北海+涠洲岛6日⊙赠赶海+海底世界/住香格里拉+船票+上岛费'
+
+      },
+      {
+        id: '003',
+        imgUrl: '//imgs.qunarzz.com/vs_ceph_vs_tts/e802ce19-7950-419c-b790-39fb364cf7f6.jpg_r_480x320x90_b8313d0d.jpg',
+        title: '北海当地游 6天自由行',
+        desc: '🔥火爆海岛游⊙北海+涠洲岛6日⊙赠赶海+海底世界/住香格里拉+船票+上岛费'
+
+      },
+      {
+        id: '004',
+        imgUrl: '//imgs.qunarzz.com/vs_ceph_vs_tts/e802ce19-7950-419c-b790-39fb364cf7f6.jpg_r_480x320x90_b8313d0d.jpg',
+        title: '北海当地游 6天自由行',
+        desc: '🔥火爆海岛游⊙北海+涠洲岛6日⊙赠赶海+海底世界/住香格里拉+船票+上岛费'
+
+      }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/mixins.styl';
+.title
+  margin-top: 0.2rem
+  line-height: 0.8rem
+  background:#eee
+  text-indent: 0.2rem
+.item
+  overflow: hidden
+  display: flex
+  height: 1.9rem
+  .item-img
+    width: 1.7rem
+    height: 1.7rem
+    padding: 0.1rem
+  .item-info
+    flex: 1
+    padding: 0.1rem
+    min-width:0
+    .item-title
+      font-weight: 500
+      line-height: 0.54rem
+      font-size: 0.32rem
+      ellipsis()
+    .item-desc
+      line-height: 0.4rem
+      color: #ccc
+      ellipsis()
+    .item-button
+      line-height: 0.44rem;
+      margin-top: 0.2rem
+      background: #ff9300
+      padding: 0 0.2rem
+      border-radius: 0.06rem
+      color: #fff
+
+</style>
---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,84 @@
+<template>
+  <div>
+    <div class="title">周末去哪儿</div>
+    <ul>
+      <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+         <div class="item-img-weekend">
+            <img class="item-img" :src="item.imgUrl">
+         </div>
+        <div class="item-info">
+          <p class="item-title">{{ item.title }}</p>
+          <p class="item-desc">{{ item.desc }}</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeWeekend',
+  data () {
+    return {
+      recommendList: [
+        {
+          id: '001',
+          imgUrl: 'https://tr-osdcp.qunarzz.com/tr-osd-tr-space/img/d42b4e9256a0ad0ad10f5e10b041cc1e.jpg_r_680x453x95_23c9c231.jpg',
+          title: '北海当地游 6天自由行',
+          desc: '🔥火爆海岛游⊙北海+涠洲岛6日⊙赠赶海+海底世界/住香格里拉+船票+上岛费'
+
+        },
+        {
+          id: '002',
+          imgUrl: 'https://tr-osdcp.qunarzz.com/tr-osd-tr-space/img/d42b4e9256a0ad0ad10f5e10b041cc1e.jpg_r_680x453x95_23c9c231.jpg',
+          title: '北海当地游 6天自由行',
+          desc: '🔥火爆海岛游⊙北海+涠洲岛6日⊙赠赶海+海底世界/住香格里拉+船票+上岛费'
+
+        },
+        {
+          id: '003',
+          imgUrl: 'https://tr-osdcp.qunarzz.com/tr-osd-tr-space/img/d42b4e9256a0ad0ad10f5e10b041cc1e.jpg_r_680x453x95_23c9c231.jpg',
+          title: '北海当地游 6天自由行',
+          desc: '🔥火爆海岛游⊙北海+涠洲岛6日⊙赠赶海+海底世界/住香格里拉+船票+上岛费'
+
+        },
+        {
+          id: '004',
+          imgUrl: 'https://tr-osdcp.qunarzz.com/tr-osd-tr-space/img/d42b4e9256a0ad0ad10f5e10b041cc1e.jpg_r_680x453x95_23c9c231.jpg',
+          title: '北海当地游 6天自由行',
+          desc: '🔥火爆海岛游⊙北海+涠洲岛6日⊙赠赶海+海底世界/住香格里拉+船票+上岛费'
+
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/mixins.styl';
+.title
+  margin-top: 0.2rem
+  line-height: 0.8rem
+  background:#eee
+  text-indent: 0.2rem
+.item-img-wrapper
+  overflow: hidden
+  height: 0
+  padding-bottom: 33.9%
+.item-img
+  width: 100%
+  height: 1.7rem
+.item-info
+  padding: 0.1rem
+  .item-title
+    font-weight: 500
+    line-height: 0.54rem
+    font-size: 0.32rem
+    ellipsis()
+  .item-desc
+    line-height: 0.4rem
+    color: #ccc
+    ellipsis()
+
+</style>
